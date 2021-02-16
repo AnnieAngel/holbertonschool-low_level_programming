ff5fe2d4259102feccc2c55d85dcf73085b6fb7f
Angie Angel <2969@holbertonschool.com>
2021-02-15 20:03:52 -0500

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - This is funtion
+*Return: 0
+*/
+int main(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a != 'e' && a != 'q')
+{
+putchar (a);
+}
+}
+putchar('\n');
+return (0);
+}
