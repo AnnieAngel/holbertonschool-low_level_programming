37d3499b61d5029ae5a6837f3c19138dc1e4dddc
Angie Angel <2969@holbertonschool.com>
2021-02-16 18:56:16 -0500

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - This is funtion
+*Return: 0
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
