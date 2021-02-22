27c1fb28f74ac6ef9f34aeb682dbf878cab1e8a7
Angie Angel <2969@holbertonschool.com>
2021-02-22 15:57:08 -0500

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <ctype.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+    if (islower(c) == 0)
+    {
+        return (0);     
+    }
+    else 
+    {
+        return (1);
+    }
+}
